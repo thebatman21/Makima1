55a67617e24de7a4745f4dc4caf4b2796b1bc9d1
Roronoa Zoro <ramakrishnan4510@gmail.com>
2021-04-02 17:02:15 +0530

    I am sure this is last. Testing one last time updating dwarn in warns.py

--- SungJinwooRobot/modules/warns.py
+++ SungJinwooRobot/modules/warns.py
@@ -173,7 +173,13 @@ def warn_user(update: Update, context: CallbackContext) -> str:
     user_id, reason = extract_user_and_text(message, args)
     if message.text.startswith("/d") and message.reply_to_message:
         message.reply_to_message.delete()
-        return warn(chat.get_member(user_id).user, chat, reason, message, warner)
+        return warn(
+            message.reply_to_message.from_user,
+            chat,
+            reason,
+            message.reply_to_message,
+            warner,
+        )
     if user_id:
         if (
             message.reply_to_message

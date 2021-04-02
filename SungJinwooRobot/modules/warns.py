b1746be2b47dd1ed92c5e3174bfdc336e88ce4e3
Roronoa Zoro <ramakrishnan4510@gmail.com>
2021-04-02 14:22:38 +0530

    Successfully failed updating dwarn in warns.py

--- SungJinwooRobot/modules/warns.py
+++ SungJinwooRobot/modules/warns.py
@@ -173,15 +173,20 @@ def warn_user(update: Update, context: CallbackContext) -> str:
     user_id, reason = extract_user_and_text(message, args)
     if message.text.startswith("/d") and message.reply_to_message:
         message.reply_to_message.delete()
-        return warn(
-            chat.get_member(user_id).user, chat, reason, message, warner)
     if user_id:
-        if message.reply_to_message and message.reply_to_message.from_user.id == user_id:
-            return warn(message.reply_to_message.from_user, chat, reason,
-                        message.reply_to_message, warner)
-        else:
+        if (
+            message.reply_to_message
+            and message.reply_to_message.from_user.id == user_id
+        ):
             return warn(
-                chat.get_member(user_id).user, chat, reason, message, warner)
+                message.reply_to_message.from_user,
+                chat,
+                reason,
+                message.reply_to_message,
+                warner,
+            )
+        else:
+            return warn(chat.get_member(user_id).user, chat, reason, message, warner)
     else:
         message.reply_text("That looks like an invalid User ID to me.")
     return ""

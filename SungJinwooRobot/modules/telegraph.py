c0e47184e0b31fbaa1cae08efb300b29c5f370ec
Roronoa Zoro <73102278+Pirate-Hunter@users.noreply.github.com>
2021-04-20 20:59:11 +0530

    Update telegraph.py

--- SungJinwooRobot/modules/telegraph.py
+++ SungJinwooRobot/modules/telegraph.py
@@ -12,7 +12,7 @@ from telethon import events
 from SungJinwooRobot.services.telethon import tbot as borg
 
 telegraph = Telegraph()
-r = telegraph.create_account(short_name="DaisyX")
+r = telegraph.create_account(short_name="SungJin-Woo")
 auth_url = r["auth_url"]
 
 # Will change later

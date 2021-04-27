6b6557df47543bd5802224a2537a0b5f211fdbe5
Roronoa Zoro <73102278+Pirate-Hunter@users.noreply.github.com>
2021-04-27 14:53:31 +0530

    Update karma.py

--- SungJinwooRobot/modules/karma.py
+++ SungJinwooRobot/modules/karma.py
@@ -1,4 +1,5 @@
 from SungJinwooRobot import pgram
+from SungJinwooRobot.modules.disable import DisableAbleCommandHandler
 from SungJinwooRobot.utils.dbfunctions import (update_karma, get_karma, get_karmas,
                                    int_to_alpha, alpha_to_int)
 from SungJinwooRobot.utils.filter_groups import karma_positive_group, karma_negative_group

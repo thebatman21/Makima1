a6b1ae95c558dc101cd2c080214400d4d755b5b4
Hirojazz <79592216+Hirojazz@users.noreply.github.com>
2021-05-02 23:58:57 +0530

    Update __main__.py

--- SungJinwooRobot/__main__.py
+++ SungJinwooRobot/__main__.py
@@ -115,7 +115,7 @@ CHAT_SETTINGS = {}
 USER_SETTINGS = {}
 
 for module_name in ALL_MODULES:
-    imported_module = importlib.import_module("KURUMIBOT.modules." + module_name)
+    imported_module = importlib.import_module("SungJinwooRobot.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
         imported_module.__mod_name__ = imported_module.__name__
 

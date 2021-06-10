60c357d95322e30b81b50561e3c2f1f8b8aae3e6
thebatman21 <80282888+thebatman21@users.noreply.github.com>
2021-06-10 12:15:48 +0530

    Update __main__.py

--- SungJinwooRobot/__main__.py
+++ SungJinwooRobot/__main__.py
@@ -208,7 +208,7 @@ def start(update: Update, context: CallbackContext):
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(
-                    escape_markdown(first_name),
+                    escape_markdown(first_name)),
                 parse_mode=ParseMode.MARKDOWN,
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(buttons),
